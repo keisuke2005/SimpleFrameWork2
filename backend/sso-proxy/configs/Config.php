59c013718168c755a4d9758d70c92f5410b4787b
keisuke.ueda <ukei2021@gmail.com>
2023-05-20 23:05:02 +0900

    ip削除

--- backend/sso-proxy/configs/Config.php
+++ backend/sso-proxy/configs/Config.php
@@ -12,12 +12,8 @@ class Config
     const LOGIN_URL = "/sso-auth/login.html";
 
 
-    //const SYSTEM_MONNITOR_DOMAIN = 'https://153.127.41.38';
-    
-
-
     const SYSTEM_MONNITOR_SCHEME = 'https://';
-    const SYSTEM_MONNITOR_DOMAIN = '153.127.41.38';
+    const SYSTEM_MONNITOR_DOMAIN = 'localhost';
     const SYSTEM_MONNITOR_PORT = '';
     const SYSTEM_MONNITOR_BASEURI = '/sso-proxy/systemMonitor';
     const SYSTEM_MONITOR_LOGIN_URL = '/zabbix/index.php';
